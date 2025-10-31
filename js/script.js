5cc4364b64a2b87cc46df5eaa280a3ec4fba639e
pedrogperez1989-cpu <pedrogperez1989@gmail.com>
2025-10-31 07:39:29 +0000

    Contenido del proyecto
    se ha añadido a la carpeta el proyecto web

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const boton = document.getElementById("botonIA");
+boton.addEventListener('click', function() {
+  alert("funcion no disponible !");
+});
+
+const consultaES = document.getElementById("consultaES");
+consultaES.addEventListener('click', function() {
+  alert("No podemos atenderlo en este momento!");
+});
+
+const consultaIE = document.getElementById("consultaIE");
+consultaIE.addEventListener('click', function() {
+  alert("No podemos atenderlo en este momento!");
+});
